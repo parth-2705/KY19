ff5b60c06031d437d0d0f20981066d8756d40743
parth-2705 <40501175+parth-2705@users.noreply.github.com>
2018-10-15 12:44:29 +0530

    Update index.js

--- ky19/js/index.js
+++ ky19/js/index.js
@@ -1,6 +1,18 @@
 
 $(document).ready(function() {
-    
+ $(window).load(function(){
+
+  $('#gif').fadeOut(2000, function(){
+
+  $('#loading').fadeOut(2000);
+  
+
+});
+  
+
+});
+
+	
     //activate wow.js
      new WOW().init();
   
@@ -12,5 +24,42 @@ $(document).ready(function() {
     });
   
 
-  
+  var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+	
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+	
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
+
+    $('a.control_next').click(function () {
+        moveRight();
+    });
+
+});
 });
